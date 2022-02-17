11ef75f9f415e9df7e6cd72c6272fcb91ab0b97d
Sabr <sbrsuleiman@gmail.com>
2022-02-16 23:45:42 -0500

    created app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+
+const BACKGROUND_COLOR: Color = Color::rgb(1.0, 1.0, 1.0);
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .insert_resource(WindowDescriptor {
+            title: String::from("TETRIS"),
+            width: 720.0, 
+            height: 1280.0,
+            ..Default::default()
+        })
+        .run();
 }
